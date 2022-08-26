e5ad336b459a8ad2cfd60260df874a99511c7791
witrikomalasari <dede.witri@gmail.com>
2022-08-26 20:11:30 +0700

    add struktur folder w/ atomic design pattern

--- Luvets_Shop/src/Roots/RootStack.js
+++ Luvets_Shop/src/Roots/RootStack.js
@@ -1,15 +1,17 @@
+import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import {SplashScreen} from '../Pages';
 
 const Stack = createStackNavigator();
 
 const RootStack = () => {
   return (
     <Stack.Navigator>
-      {/*
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Profile" component={Profile} />
-      <Stack.Screen name="Settings" component={Settings} />
-  */}
+      <Stack.Screen
+        name="SplashScreen"
+        component={SplashScreen}
+        options={{headerShown: false}}
+      />
     </Stack.Navigator>
   );
 };
---
+++ Luvets_Shop/src/Utils/Colors/index.js
@@ -0,0 +1,19 @@
+const mainColors = {
+  white: '#ffffff',
+  black: '#000000',
+};
+
+export const colors = {
+  background: {
+    white: mainColors.white,
+    black: mainColors.black,
+  },
+  text: {
+    active: mainColors,
+    inActive: mainColors,
+  },
+  border: {},
+  button: {
+    disable: {},
+  },
+};
